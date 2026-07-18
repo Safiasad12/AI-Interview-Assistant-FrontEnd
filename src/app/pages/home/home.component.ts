193d4566fa5c70b80adee6f75fcdca3cadc3f8c5
safi <safiasad12@gmail.com>
2026-07-19 04:30:35 +0530

    feat: add landing page and navigation for AI Interview Assistant

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ 
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router) {}
+
+  startInterview() {
+  this.router.navigate(['/upload']);
+}
 
 }
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
